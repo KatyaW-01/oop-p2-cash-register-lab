39c152e5db9304f4726783a81d71d60d75d562f9
Katya Weicht <katya.weicht@gmail.com>
2025-07-08 14:04:47 -0600

    Create apply discount method

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -22,3 +22,14 @@ class CashRegister:
     self.total += price * quantity
     self.items.append(item)
     self.previous_transactions.append({"item":item, "price":price, "quantity":quantity})
+
+  def apply_discount(self):
+    if len(self.previous_transactions) == 0 or self.discount == 0:
+      print("There is no discount to apply.")
+    else:
+      discount_percentage = self.discount/100
+      price_off = self.total * discount_percentage
+      self.total -= price_off
+      self.total = round(self.total)
+      print(f"After the discount, the total comes to ${self.total}.")
+      self.previous_transactions.pop()
