d828867ae5a6a7fc2d83c354646490073f92f7e2
Katya Weicht <katya.weicht@gmail.com>
2025-07-08 12:32:04 -0600

    Set up class attributes

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self,discount=0,total=0,items=[],previous_transactions=[]):
+    self.discount = discount
+    self.total = total
+    self.items = items
+    self.previous_transactions = previous_transactions
