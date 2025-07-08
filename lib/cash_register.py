b3ab1521eeb8853e058cf987f4b653db333bb438
Katya Weicht <katya.weicht@gmail.com>
2025-07-08 13:28:33 -0600

    Validate that discount is between 0 and 100

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -6,3 +6,14 @@ class CashRegister:
     self.total = total
     self.items = items
     self.previous_transactions = previous_transactions
+
+  @property
+  def discount(self):
+    return self._discount
+  
+  @discount.setter
+  def discount(self,value):
+    if 0<= value <= 100:
+      self._discount = value
+    else:
+      print("Not valid discount")
